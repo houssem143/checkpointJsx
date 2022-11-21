0d68f44635e8cdbce3f5d2a21f3622206b26c23f
houssem143 <houssem14392@gmail.com>
2022-11-21 12:38:53 +0100

    adding jsx

---
+++ src/components/Profile/FullName.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const FullName = () => {
+  return (
+    <div>
+        <h1>First.Name: Houssem
+            
+        </h1>
+        <h1>
+            Last.Name: Mguidich
+        </h1>
+    </div>
+  )
+}
+
+export default FullName
---
+++ src/components/Profile/ProfilePhoto.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import img from "../image/jh.jpg"
+
+const ProfilePhoto = () => {
+  return (
+    <div style={{marginRight:"80%" , marginTop:"-18%"}}>
+        <img width={"60%"} src={img} alt='profile'/>
+    </div>
+  )
+}
+
+export default ProfilePhoto
